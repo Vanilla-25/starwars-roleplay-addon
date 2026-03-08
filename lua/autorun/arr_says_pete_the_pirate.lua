f760b8dfd779f305f1103733f305bad7b03b5fd1
Vanilla <164254086+Vanilla-25@users.noreply.github.com>
2026-03-09 02:02:15 +1000

    Starwars-Roleplay-Addon

---
+++ lua/autorun/arr_says_pete_the_pirate.lua
@@ -0,0 +1,8 @@
+
+local Category = "CIS Gunship"
+
+local NPC = { 	Name = "CIS Gunship", 
+				Class = "npc_apache",
+				Category = Category	}
+
+list.Set( "NPC", NPC.Class, NPC )
---
+++ lua/entities/proj_dan_heli_shot/shared.lua
@@ -0,0 +1,12 @@
+ ENT.Type = "anim"  
+ ENT.Base = "base_gmodentity"     
+ ENT.PrintName		= "Bolter Shot"  
+ ENT.Author			= ""  
+ ENT.Contact			= ""  
+ ENT.Purpose			= ""  
+ ENT.Instructions	= ""  
+ 
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
+ENT.exploding			= false
+
