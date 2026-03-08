f28c1b026ee2374ee97725bcb69b3e0d58f5238f
Vanilla <164254086+Vanilla-25@users.noreply.github.com>
2026-03-09 03:00:04 +1000

    Delete lua directory

--- lua/entities/dropship_ent/shared.lua
+++
@@ -1,5 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-ENT.PrintName = "NPC Dropship"
-ENT.Category = "Dropship Test"
-ENT.Spawnable = false
--- lua/ulx/modules/sh/cc_cvars.lua
+++
@@ -1,6 +0,0 @@
----------------
---  ConVars  --
----------------
-
-CreateConVar("donate_url", "http://www.google.com");
-CreateConVar("explode_ragdolls", "0");
